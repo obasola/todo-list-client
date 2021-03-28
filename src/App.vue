87d8edf982975f89ee5665f59aa680218e060333
eusdart98 <dthompson98@gmail.com>
2021-03-28 18:22:00 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,52 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="http://10.170.1.105">
+        <img src="./assets/yoruba-symbol-2.png" width="80" height="50" /> Todo App
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" style="color: #000000; text-decoration: none">Home&nbsp;&nbsp;</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" style="color: #000000; text-decoration: none">About</router-link>
+          </li>
+ 
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Portfolio
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Address</a></li>
+              <li><a class="dropdown-item" href="#">City</a></li>
+              <li><a class="dropdown-item" href="#">Country</a></li>
+              <li><a class="dropdown-item" href="#">Customer</a></li>
+              <li><a class="dropdown-item" href="#">Store</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Todo</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
   </div>
   <router-view />
 </template>
